9bff162b9572760dd6f058edc61dfaf7ae3a2a92
franknoh <franknoh1218@naver.com>
2021-08-26 08:53:54 +0900

    뭔가 이상하지만 일단 커밋

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 var pdf = require('html-pdf');
 const del = require('del');
-const log = console.log;
 
-//________________________________________
 options = {
   "height": "297mm",
   "width": "210mm",
@@ -20,9 +18,6 @@ options = {
   timeout: 86400000,
 }
 const start = Date.now()
-// 폴더 생성
-//________________________________________
-
 
 const getHtml = async (url) => {
   try {
@@ -69,57 +64,74 @@ getHtml('/wiki/index.php/AIME_Problems_and_Solutions')
       }*/
     });
     return lists;
-  })
-  .then(async res => {
-    log(res)
-    res.forEach(e=>{
-    //if(true){
-      //e=res[2]
-      if (![0, 1].includes(res.indexOf(e))){
-      let all=[];
-      let allhtml='';
-      for (let f = 1; f < 16; f++) {
-        getHtml(e.url + '_Problems/Problem_' + f).then(html => {
-          const $ = cheerio.load(html.data);
-          const $bodyList = $("div.page-wrapper");
-          $('div.toc').remove();
-          $('span#See_Also').remove();
-          $('div#main-footer').remove();
-          $("table").last().remove()
-          $("div.page-wrapper p").last().remove()
-          $("div.printfooter").remove();
-          $("div.catlinks").remove();
-          //htmldata='<!doctype html><html><head><title>김치볶음밥소고기</title></head><body>'+$bodyList.html().split('//').join('http://')+'</body></html>'
-          //fs.writeFileSync('index.html',htmldata);
-          log(e.name+'-'+f);
-          fs.writeFileSync('./html/'+e.name+'-'+f+'.html',$bodyList.html().split('//').join('http://').split('https:http://').join('https://').split('"/wiki').join('"https://artofproblemsolving.com/wiki').split('https://wiki-images.artofproblemsolving.comhttp://').join('https://wiki-images.artofproblemsolving.com//'))
-          all.push(e.name+'-'+f+'.html')
-          // pdf.create($bodyList.html().split('//').join('http://').split('https://http').join('https'), options).toFile('./pdf/'+e.name+'/'+e.name+'-'+f+'.pdf', function(err, res) {
-          //   if (err) return console.log(err);
-          //   console.log(res);
-          // });
-          if(all.length==15){
-            all=[];
-            for(mn=1;mn<16;mn++){
-              all.push(e.name+'-'+mn+'.html')
-            }
-            all.forEach((el)=>{
-              html = fs.readFileSync('./html/'+el, "utf8")
-              allhtml+=html+'\n\n\n'
-              if(el==all[all.length-1]){
-                pdf.create(allhtml, options).toFile('./pdf/'+e.name+'.pdf', function(err, res) {
-                  if (err) return console.log(err);
-                  console.log(res);
-                  if(e.name==e[e.length-1].name){
-                    const stop = new Date()
-                    console.log(`${(stop - start)/1000} seconds`)
-                  }
-                });
-              }
-            })
-          }
+  }).then(async res => {
+    console.log(res)
+    let done=0;
+
+    async function gen_pdf(n){
+      if(n>=res.length){
+        console.log({
+          message: 'finished'
+        })
+      }else{
+        gen(n).then((res)=>{
+          setTimeout(() => {
+            gen_pdf(n+1)
+          }, 1000*60*4);
         })
       }
     }
-  })
+    async function gen(num){
+      const e=res[num]
+      console.log(e)
+      if (![0, 1].includes(res.indexOf(e))){
+        let all=[];
+        let allhtml=''
+        for (let f = 1; f < 16; f++) {
+          getHtml(e.url + '_Problems/Problem_' + f).then(html => {
+            const $ = cheerio.load(html.data);
+            const $bodyList = $("div.page-wrapper");
+            $('div.toc').remove();
+            $('span#See_Also').remove();
+            $('div#main-footer').remove();
+            $("table").last().remove()
+            $("div.page-wrapper p").last().remove()
+            $("div.printfooter").remove();
+            $("div.catlinks").remove();
+            let templog={
+              name: e.name,
+              url: e.url + '_Problems/Problem_' + f,
+              num: f
+            }
+            console.log(templog)
+            htmll=$bodyList.html().split('//').join('http://').split('https:http://').join('https://').split('"/wiki').join('"https://artofproblemsolving.com/wiki').split('https://wiki-images.artofproblemsolving.comhttp://').join('https://wiki-images.artofproblemsolving.com//')
+            htmldata=htmll
+            fs.writeFileSync('./html/'+e.name+'-'+f+'.html',htmldata)
+            all.push(e.name+'-'+f+'.html')
+            if(all.length==15){
+              all=[];
+              for(mn=1;mn<16;mn++){
+                all.push(e.name+'-'+mn+'.html')
+              }
+              all.forEach((el)=>{
+                html = fs.readFileSync('./html/'+el, "utf8")
+                allhtml+=html+'\n\n\n'
+                if(el==all[all.length-1]){
+                  pdf.create(allhtml, options).toFile('./pdf/'+e.name+'.pdf', function(err, result) {
+                    if (err) return console.log(err);
+                    console.log(result);
+                    done++
+                    if(done==res.length-2){
+                      const stop = new Date()
+                      console.log(`${(stop - start)/1000} seconds`)
+                    }
+                  });
+                }
+              })
+            }
+          })
+        }
+      }
+    }
+    gen_pdf(2)
   })
